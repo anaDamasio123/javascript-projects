02ce63d1d1f191ac9c871df5eda21a979f21a792
anadamasio <2051320@student.uma.pt>
2023-03-04 22:54:51 +0000

    the active classes is passing

--- pig_game/script.js
+++ pig_game/script.js
@@ -8,13 +8,32 @@ document.querySelector('.dice').classList.add('hidden');
 const rollDice = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 const diceImages = document.querySelector('.dice');
+const players = document.querySelectorAll('.player');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
+let score = 0;
+let randomNumber;
+let activePlayer;
 
 
 // Rolling the dice
 rollDice.addEventListener('click', function() {
     diceImages.classList.remove('hidden');
-    let randomNumber = Math.trunc(Math.random() * 6) + 1;
+    randomNumber = Math.trunc(Math.random() * 6) + 1;
     diceImages.src = `dice-${randomNumber}.png`;
+    for (let index = 0; index < players.length; index++) {
+        document.getElementById(`current--${index}`).textContent = 0;
+        if(players[index].classList.contains('player--active')) {
+            score = score + randomNumber;
+            document.getElementById(`current--${index}`).textContent = score;
+        }
+    }
 });
 
 // ver o toggle
+holdButton.addEventListener('click', function() {
+    score = 0;
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
+});
